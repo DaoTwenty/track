c1728f262b60812324fdd34ae23d6c856801137a
Paul Triana <101296375+DaoTwenty@users.noreply.github.com>
2025-07-24 04:25:00 +0200

    Update track.rb

--- Formula/track.rb
+++ Formula/track.rb
@@ -2,7 +2,7 @@ class Track < Formula
   desc "CLI project time tracking tool"
   homepage "https://github.com/DaoTwenty/track"
   url "https://github.com/DaoTwenty/track/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "616a1504804b7c6b909cb6fe8b465bf425de6dfc262d42bee60046768cbcb566"
+  sha256 "da03022729904432a83da7de2b28c945eacef15d770ea2dfd71d27ca1ae182ae"
   license "MIT"
 
   def install
